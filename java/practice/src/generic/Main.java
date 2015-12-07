c7e9f03e44d0cc6314f5ae98d6acaf290242b4e2
Kim Dahye <dahye.kim@nhnnext.org>
2015-12-07 18:46:47 +0900

    [Effective Java 27]generic singleton factory pattern

---
+++ java/practice/src/generic/Main.java
@@ -0,0 +1,25 @@
+package generic;
+
+/**
+ * Created by sophie on 2015. 12. 7..
+ */
+public class Main {
+    private static UnaryFunction<Object> IDENTITY_FUNCTION = new UnaryFunction<Object>() {
+        @Override
+        public Object apply(Object arg) {
+            return arg;
+        }
+    };
+
+    public static <T> UnaryFunction<T> identityFunction() {
+        return (UnaryFunction<T>) IDENTITY_FUNCTION;
+    }
+
+    public static void main (String[] args) {
+        String[] strings = {"1", "b", "c"};
+        UnaryFunction<String> sameString = identityFunction();
+        for(String s : strings) {
+            System.out.println(sameString.apply(s));
+        }
+    }
+}
---
+++ java/practice/src/generic/UnaryFunction.java
@@ -0,0 +1,9 @@
+package generic;
+
+/**
+ * Created by sophie on 2015. 12. 7..
+ */
+public interface UnaryFunction<T> {
+    T apply (T arg);
+}
+
